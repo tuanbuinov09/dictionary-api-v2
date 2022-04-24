e1017f01202cc1ab2b30d1b9b19938ff9d4fbbd1
Lưu Tiến Phát <luutienphat@gmail.com>
2022-04-24 23:33:31 +0700

    Delete unused class

--- src/main/java/com/example/demo/controllers/repository/EnWordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controllers.repository;
-
-import com.example.demo.controllers.entities.EnWord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnWordRepository extends JpaRepository<EnWord, Long> {
-
-}
