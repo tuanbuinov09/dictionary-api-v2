d131a2b50f73097890aa1ce0d5035eacd1b63a21
Lưu Tiến Phát <luutienphat@gmail.com>
2022-05-04 23:39:48 +0700

    Declare the method body to get the user by username

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import com.example.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	User findByUsername(String username);
 }
